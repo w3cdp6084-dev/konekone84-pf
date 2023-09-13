74136f15334499e3607548dd0fff16b1a725ff7d
w3cdp6084-dev <yusukew3@gmail.com>
2023-09-13 10:52:06 +0900

    folderadd resetcssadd

--- app.vue
+++ app.vue
@@ -1,49 +1,16 @@
 <template>
   <div id="scene-container"></div>
+  <MenuButton />
 </template>
 
-<script setup>
-import { onMounted } from 'vue'
-import * as THREE from 'three'
-import { gsap } from 'gsap'
+<script>
+import MenuButton from '~/components/MenuButton.vue'
 
-onMounted(() => {
-  // Three.jsの基本設定（シーン、カメラ、レンダラー）
-  const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-  const renderer = new THREE.WebGLRenderer()
-
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  document.getElementById('scene-container').appendChild(renderer.domElement)
-
-  // ドットの生成
-  const geometry = new THREE.BufferGeometry().setFromPoints([
-    new THREE.Vector3(-1, 0, 0),
-    new THREE.Vector3(0, 1, 0),
-    new THREE.Vector3(1, 0, 0),
-  ])
-
-  const material = new THREE.PointsMaterial({ color: 0xff0000, size: 0.1 })
-  const points = new THREE.Points(geometry, material)
-
-  scene.add(points)
-
-  // GSAPによるアニメーション
-  gsap.to(points.rotation, {
-    duration: 2,
-    x: Math.PI * 2,
-    ease: 'none',
-    repeat: -1
-  })
-
-  // アニメーションループ
-  const animate = () => {
-    requestAnimationFrame(animate)
-    renderer.render(scene, camera)
+export default {
+  components: {
+      MenuButton
   }
-
-  animate()
-})
+}
 </script>
 
 <style>
---
+++ components/MenuButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <button
+        class="button-menu"
+        :aria-label="open ? 'メニューを閉じる' : 'メニューを開く'"
+        :aria-pressed="open"
+        @click="toggleMenu"
+        type="button">
+
+        <!-- 中略 -->
+
+    </button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            open: false
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.open = !this.open;
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* ここにCSSを貼り付け */
+</style>
