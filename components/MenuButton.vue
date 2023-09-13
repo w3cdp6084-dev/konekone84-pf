d3aaa9f7c68357907ee0c8d3635c104fbe95bed3
w3cdp6084-dev <yusukew3@gmail.com>
2023-09-13 14:37:52 +0900

    Menu add

--- components/MenuButton.vue
+++ components/MenuButton.vue
@@ -5,9 +5,21 @@
         :aria-pressed="open"
         @click="toggleMenu"
         type="button">
-
-        <!-- 中略 -->
-
+        <span class="button-menu__icons">
+            <span class="button-menu__dots">
+                <span class="button-menu__dot"></span>
+                <span class="button-menu__dot"></span>
+                <span class="button-menu__dot"></span>
+            </span>
+            <span class="button-menu__bars">
+                <span class="button-menu__bar"><span class="button-menu__bar-inner"></span></span>
+                <span class="button-menu__bar"><span class="button-menu__bar-inner"></span></span>
+            </span>
+	    </span>
+        <span class="global-header__menu-trigger__label-wrapper" aria-hidden="true">
+            <span class="global-header__menu-trigger__label text-en-mobile-label--l text-en-desktop-label--l ">Menu</span>
+            <span class="global-header__menu-trigger__label text-en-mobile-label--l text-en-desktop-label--l ">Close</span>
+        </span>
     </button>
 </template>
 
