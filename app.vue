6ef80cde3ae0908fa38b5d2bdbdba411688e2f79
w3cdp6084-dev <yusukew3@gmail.com>
2023-11-18 14:18:04 +0900

    menu animation test

--- app.vue
+++ app.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
+    <Menu />
     <NuxtWelcome />
   </div>
 </template>
 <script>
 import * as THREE from 'three';
 import { gsap } from 'gsap';
+import Menu from '@/components/Menu.vue'
+export default {
+  components: {
+    Menu
+  }
+}
 </script>
 <style lang="scss">
 // ここにSCSSスタイルを書く
--- components/Menu.vue
+++ components/Menu.vue
@@ -1,7 +1,7 @@
 <template>
  <nav class="menu">
    <ul>
-     <li v-for="(item, index) in menuItems" :key="index">
+     <li v-for="(item, index) in menuItems" :key="index" @mouseover="hoverStart" @mouseleave="hoverEnd">
        {{ item.name }}
      </li>
    </ul>
@@ -9,20 +9,32 @@
 </template>
 
 <script>
+import * as THREE from 'three';
+import { gsap } from 'gsap';
+
 export default {
- data() {
-   return {
-     menuItems: [
-       { name: 'Home' },
-       { name: 'About' },
-       { name: 'Services' },
-       { name: 'Contact' },
-     ],
-   };
- },
+  data() {
+    return {
+      menuItems: [
+        { name: 'Home', path: '/' },
+        { name: 'About', path: '/about' },
+        { name: 'Services', path: '/services' },
+        { name: 'Contact', path: '/contact' },
+        // 他のメニュー項目を追加
+      ],
+    };
+  },
+  methods: {
+    hoverStart(event) {
+      gsap.to(event.target, { scale: 1.2, duration: 0.3 });
+    },
+    hoverEnd(event) {
+      gsap.to(event.target, { scale: 1, duration: 0.3 });
+    },
+  },
 };
 </script>
 
-<style scoped>
-
+<style lang="scss">
+// ここにSCSSスタイルを書く
 </style>
