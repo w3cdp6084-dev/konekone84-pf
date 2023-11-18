6b334b9406c356dc4dca9e3555e911a1f6290b29
w3cdp6084-dev <yusukew3@gmail.com>
2023-11-19 06:33:11 +0900

    redesign

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,11 +1,12 @@
 <template>
- <nav class="menu">
-   <ul>
-     <li v-for="(item, index) in menuItems" :key="index" @mouseover="hoverStart" @mouseleave="hoverEnd">
-       {{ item.name }}
-     </li>
-   </ul>
- </nav>
+  <button @click.prevent="showMenu">Menu</button>
+  <nav class="menu" v-if="menuVisible">
+    <ul>
+      <li v-for="(item, index) in menuItems" :key="index" @mouseover="hoverStart" @mouseleave="hoverEnd">
+        <a :href="item.path">{{ item.name }}</a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
@@ -22,19 +23,77 @@ export default {
         { name: 'Contact', path: '/contact' },
         // 他のメニュー項目を追加
       ],
+      menuVisible: false,
     };
   },
   methods: {
-    hoverStart(event) {
-      gsap.to(event.target, { scale: 1.2, duration: 0.3 });
-    },
-    hoverEnd(event) {
-      gsap.to(event.target, { scale: 1, duration: 0.3 });
+    showMenu() {
+      this.menuVisible = !this.menuVisible;
+      this.$nextTick(() => {
+        if (this.menuVisible) {
+          gsap.from(this.$el, { opacity: 0, y: '-100%', duration: 0.5 });
+        } else {
+          gsap.to(this.$el, { opacity: 0, y: '-100%', duration: 0.5 });
+        }
+      });
     },
   },
 };
 </script>
 
 <style lang="scss">
-// ここにSCSSスタイルを書く
+.menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  height: 300px; /* 円の直径 */
+  width: 300px; /* 円の直径 */
+  position: relative;
+}
+.menu ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  height: 300px; /* 円の直径 */
+  width: 300px; /* 円の直径 */
+  position: relative;
+}
+
+.menu li {
+  position: absolute;
+  width: 100px; /* メニュー項目の幅 */
+  height: 100px; /* メニュー項目の高さ */
+  text-align: center;
+  line-height: 100px; /* メニュー項目の高さと同じにする */
+}
+
+.menu li:nth-child(1) {
+  top: 50%;
+  left: 0;
+  transform: translate(-50%, -50%);
+}
+
+.menu li:nth-child(2) {
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.menu li:nth-child(3) {
+  top: 50%;
+  left: 100%;
+  transform: translate(-50%, -50%);
+}
+
+.menu li:nth-child(4) {
+  top: 100%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
