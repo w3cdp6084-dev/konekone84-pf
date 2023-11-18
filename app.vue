bdfa2a18f03677504a271521f0438725b5fd46ec
w3cdp6084-dev <yusukew3@gmail.com>
2023-11-18 13:43:26 +0900

    Menu add

--- app.vue
+++ app.vue
@@ -3,3 +3,10 @@
     <NuxtWelcome />
   </div>
 </template>
+<script>
+import * as THREE from 'three';
+import { gsap } from 'gsap';
+</script>
+<style lang="scss">
+// ここにSCSSスタイルを書く
+</style>
---
+++ components/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+ <nav class="menu">
+   <ul>
+     <li v-for="(item, index) in menuItems" :key="index">
+       {{ item.name }}
+     </li>
+   </ul>
+ </nav>
+</template>
+
+<script>
+export default {
+ data() {
+   return {
+     menuItems: [
+       { name: 'Home' },
+       { name: 'About' },
+       { name: 'Services' },
+       { name: 'Contact' },
+     ],
+   };
+ },
+};
+</script>
+
+<style scoped>
+
+</style>
